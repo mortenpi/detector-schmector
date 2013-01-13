453edf4dd1fe69dec79fe602ab90cb32a0e6fa3f
Morten Piibeleht <morten.piibeleht@gmail.com>
2013-01-13 02:42:44 +0200

    Add circle drawing.

--- analysis/plotpoints.py
+++ analysis/plotpoints.py
@@ -15,4 +15,10 @@ y = [d[1] for d in data]
 mp.scatter(x,y)
 mp.ylim([-0.5,0.5])
 mp.xlim([0,1.5])
+
+cr1 = mp.Circle((0.624986, -0.625014), 0.883883, color='r', fill=False)
+cr2 = mp.Circle((-0.623383, 0.630122), 0.890109, color='b', fill=False)
+mp.gca().add_artist(cr1)
+mp.gca().add_artist(cr2)
+
 mp.show()
