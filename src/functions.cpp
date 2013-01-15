4c5f3bcadc862c235cb4075b530a453a47b6beeb
Morten Piibeleht <morten.piibeleht@gmail.com>
2013-01-15 20:04:51 +0100

    Create functions file.

---
+++ src/functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "structures.hpp"
+
+#include "functions.hpp"
+
+using namespace std;
+
+void print_particle(particle p) {
+	cout << "Particle: " << endl;
+	cout << "  mass:   " << p.mass << endl;
+	cout << "  energy: " << p.energy << endl;
+	cout << "  angle:  " << p.phi << endl;
+	cout << "  charge: " << p.charge << endl;
+}
---
+++ src/functions.hpp
@@ -0,0 +1,6 @@
+#ifndef _FUNCTIONS_HPP_
+#define _FUNCTIONS_HPP_
+
+void print_particle(particle p);
+
+#endif
