75db626fcd94352cd0dad4eabc9f37e277adbc97
Morten Piibeleht <morten.piibeleht@gmail.com>
2013-02-02 23:57:30 +0200

    Comment and refactor functions

--- src/functions.hpp
+++ src/functions.hpp
@@ -1,6 +1,15 @@
+/**
+ * @file
+ * General purpose functions.
+ */
 #ifndef _FUNCTIONS_HPP_
 #define _FUNCTIONS_HPP_
 
+struct particle;
+
+/**
+ * Output the information about p to stdout in a human readably way.
+ */
 void print_particle(particle p);
 
 #endif
